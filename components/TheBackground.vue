29f07a734e52b5693e920bb3ff67d6dca5634a82
Krizza Bullecer <krizzabullecer@gmail.com>
2020-10-11 12:21:29 +0800

    refactor: background text

--- components/TheBackground.vue
+++ components/TheBackground.vue
@@ -3,11 +3,20 @@
     <div class="section__title">Background</div>
     <div class="section__content">
       <p>
-        I'm a Platform Engineer at an energy startup called
-        <a class="underline-link" href="https://www.exora.ph/" target="_blank"
-          >Exora Technologies</a
-        >. I help develop user interfaces provided by our awe-mazing creative
-        director using the latest front-end technologies.
+        I'm currently training to get AWS-certified in many areas. I've recently
+        achieved the foundational AWS Cloud Practitioner certification and now
+        moving on to the AWS Solutions Architect certification.
+      </p>
+
+      <p>
+        I'm a part-time IT and English teacher for students in Japan where I
+        help them learn English from a technical environment. Our lessons range
+        from the foundations of web design to deployment of full-scale web
+        applications using modern technology. I also recently started
+        freelancing as a full-stack developer at Changing Digital, a UK-based
+        software development agency. I work with other developers to create
+        websites for SMEs and large enterprises. Our stack includes React,
+        Redux, Next.js, React Native, Github Actions, and AWS.
       </p>
 
       <p>
